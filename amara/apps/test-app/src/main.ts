8d35268d41a979105540965d17c2076b86c3a79e
Alvin Espinosa <alvin.espinosa@gmail.com>
2024-05-18 21:38:16 +0800

    remove test app

--- amara/apps/test-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-console.log('>>> NX_API_URL', process.env['NX_API_URL']);
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
